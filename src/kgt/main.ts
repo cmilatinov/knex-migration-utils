a6bf714500d1007d9364fa0fd4e47301cd204cae
Cristian Milatinov <milatinovcristian@gmail.com>
2022-05-21 15:03:32 -0400

    Add more type mappings for type generator

--- src/kgt/main.ts
+++ src/kgt/main.ts
@@ -14,9 +14,16 @@ import cliOptions, { ArgsKGT, printHelp } from './cli';
 const typeMapping: { [key: string]: string } = {
     varchar: 'string',
     text: 'string',
+    int2: 'number',
     int4: 'number',
     int8: 'number',
+    int: 'number',
+    float2: 'number',
+    float4: 'number',
+    float8: 'number',
+    float: 'number',
     timestamptz: 'string',
+    timestamp: 'string',
     uuid: 'string'
 };
 
